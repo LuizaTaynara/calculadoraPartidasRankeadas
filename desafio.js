ed43d130da6e69e1a98127c6044035e6d2017dad
Luiza Santos <134156132+LuizaTaynara@users.noreply.github.com>
2026-06-10 20:04:52 +0100

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -1,1 +1,25 @@
+calculateRank(80, 20)
 
+function calculateRank(vitorias, derrotas) {
+    let sumTotal = vitorias - derrotas
+    let nivel = ""
+
+    if (vitorias <= 10) {
+        nivel = "Ferro"
+    } else if (vitorias <= 20) {
+        nivel = "Bronze"
+    } else if (vitorias <= 50) {
+        nivel = "Prata"
+    } else if (vitorias <= 80) {
+        nivel = "Ouro"
+    } else if (vitorias <= 90) {
+        nivel = "Diamante"
+    } else if (vitorias <= 100) {
+        nivel = "Lendário"
+    } else {
+        nivel = "Imortal"
+    }
+
+     console.log("O Herói tem o saldo de " + sumTotal + " vitórias e está no nível " + nivel)
+     
+}

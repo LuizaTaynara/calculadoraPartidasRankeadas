21bb9d7c7742fc36527ab184039d0c4b95599529
Luiza Santos <134156132+LuizaTaynara@users.noreply.github.com>
2026-06-10 20:20:04 +0100

    Update desafio2.js

--- desafio2.js
+++ desafio2.js
@@ -1,1 +1,26 @@
+let result = resultRank(120, 19)
+let heroi = "Eleven"
+let nivel = ""
 
+if (result <= 10) {
+    nivel = "Ferro"
+} else if (result <= 20) {
+    nivel = "Bronze"
+} else if (result <= 50) {
+    nivel = "Prata"
+} else if (result <= 80) {
+    nivel = "Ouro"
+} else if (result <= 90) {
+    nivel = "Diamante"
+} else if (result <= 100) {
+    nivel = "Lendário"
+} else {
+    nivel = "Imortal"
+}
+
+console.log(`O Herói ${heroi} tem o saldo de ${result} vitórias e está no nível ${nivel}`)
+
+function resultRank(vitorias, derrotas) {
+    let sumTotal = vitorias - derrotas
+    return sumTotal
+}
